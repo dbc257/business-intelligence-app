9476ff63ab5498a9c27813bfa7b1ac4508f3716a
David Carlisle <dave_0021@yahoo.com>
2020-07-19 15:46:07 -0500

    update

--- client/.next/static/runtime/webpack.js
+++ client/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "9734c7b3edb70b3073e5";
+/******/ 	var hotCurrentHash = "6af60f5e436de435ef31";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
