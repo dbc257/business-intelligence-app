a8ff4f940cd22766fc0b7865802335233ad738bb
David Carlisle <dave_0021@yahoo.com>
2020-07-21 13:31:43 -0500

    fixed navbar

--- client/.next/static/runtime/webpack.js
+++ client/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "ad10694e9334723791aa";
+/******/ 	var hotCurrentHash = "08b7a7a175f240a1e288";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- client/pages/charts.js
+++ client/pages/charts.js
@@ -1,142 +1,132 @@
-import react from "react"
-import { Bar, Line, Pie } from "react-chartjs-2"
+import react from "react";
+import { Bar, Line, Pie } from "react-chartjs-2";
 import Navbar from "../components/Layouts/Navbar";
 import Footer from "../components/Layouts/Footer";
-import { useRouter } from "next/router"
-
+import { useRouter } from "next/router";
 
 export default function Charts({ dataset }) {
-    const barData = {
-        labels: ["January", "February", "March", "April", "May", "June", "July"],
-        datasets: [
-            {
-                label: "Example dataset",
-                backgroundColor: "rgba(255,99,132,0.2)",
-                borderColor: "rgba(255,99,132,1)",
-                borderWidth: 1,
-                hoverBackgroundColor: "rgba(255,99,132,0.4)",
-                hoverBorderColor: "rgba(255,99,132,1)",
-                data: [65, 59, 80, 81, 56, 55, 40],
-            },
-        ],
-    };
+  const barData = {
+    labels: ["January", "February", "March", "April", "May", "June", "July"],
+    datasets: [
+      {
+        label: "Example dataset",
+        backgroundColor: "rgba(255,99,132,0.2)",
+        borderColor: "rgba(255,99,132,1)",
+        borderWidth: 1,
+        hoverBackgroundColor: "rgba(255,99,132,0.4)",
+        hoverBorderColor: "rgba(255,99,132,1)",
+        data: [65, 59, 80, 81, 56, 55, 40],
+      },
+    ],
+  };
 
-    const router = useRouter()
-    const { pid } = router
+  const router = useRouter();
+  const { pid } = router;
 
-    const lineData = {
-        labels: ["January", "February", "March", "April", "May", "June", "July"],
-        datasets: [
-            {
-                label: "Example dataset",
-                fill: false,
-                lineTension: 0.1,
-                backgroundColor: "rgba(75,192,192,0.4)",
-                borderColor: "rgba(75,192,192,1)",
-                borderCapStyle: "butt",
-                borderDash: [],
-                borderDashOffset: 0.0,
-                borderJoinStyle: "miter",
-                pointBorderColor: "rgba(75,192,192,1)",
-                pointBackgroundColor: "#fff",
-                pointBorderWidth: 1,
-                pointHoverRadius: 5,
-                pointHoverBackgroundColor: "rgba(75,192,192,1)",
-                pointHoverBorderColor: "rgba(220,220,220,1)",
-                pointHoverBorderWidth: 2,
-                pointRadius: 1,
-                pointHitRadius: 10,
-                data: [65, 59, 80, 81, 56, 55, 40],
-            },
-        ],
-    };
+  const lineData = {
+    labels: ["January", "February", "March", "April", "May", "June", "July"],
+    datasets: [
+      {
+        label: "Example dataset",
+        fill: false,
+        lineTension: 0.1,
+        backgroundColor: "rgba(75,192,192,0.4)",
+        borderColor: "rgba(75,192,192,1)",
+        borderCapStyle: "butt",
+        borderDash: [],
+        borderDashOffset: 0.0,
+        borderJoinStyle: "miter",
+        pointBorderColor: "rgba(75,192,192,1)",
+        pointBackgroundColor: "#fff",
+        pointBorderWidth: 1,
+        pointHoverRadius: 5,
+        pointHoverBackgroundColor: "rgba(75,192,192,1)",
+        pointHoverBorderColor: "rgba(220,220,220,1)",
+        pointHoverBorderWidth: 2,
+        pointRadius: 1,
+        pointHitRadius: 10,
+        data: [65, 59, 80, 81, 56, 55, 40],
+      },
+    ],
+  };
 
-    const pieData = {
-        labels: ["Red", "Blue", "Yellow"],
-        datasets: [
-            {
-                data: [300, 50, 100],
-                backgroundColor: ["#FF6384", "#36A2EB", "#FFCE56"],
-                hoverBackgroundColor: ["#FF6384", "#36A2EB", "#FFCE56"],
-            },
-        ],
-    };
+  const pieData = {
+    labels: ["Red", "Blue", "Yellow"],
+    datasets: [
+      {
+        data: [300, 50, 100],
+        backgroundColor: ["#FF6384", "#36A2EB", "#FFCE56"],
+        hoverBackgroundColor: ["#FF6384", "#36A2EB", "#FFCE56"],
+      },
+    ],
+  };
 
-    const barOptions = {
-        scales: {
-            yAxes: [
-                {
-                    scaleLabel: {
-                        display: true,
-                        labelString: "Dollar Amount",
-                    },
-                },
-            ],
-            xAxes: [
-                {
-                    scaleLabel: {
-                        display: true,
-                        labelString: "Month",
-                    },
-                },
-            ],
+  const barOptions = {
+    scales: {
+      yAxes: [
+        {
+          scaleLabel: {
+            display: true,
+            labelString: "Dollar Amount",
+          },
         },
-    };
-
-    const lineOptions = {
-        scales: {
-            yAxes: [
-                {
-                    scaleLabel: {
-                        display: true,
-                        labelString: "Dollar Amount",
-                    },
-                },
-            ],
-            xAxes: [
-                {
-                    scaleLabel: {
-                        display: true,
-                        labelString: "Month",
-                    },
-                },
-            ],
+      ],
+      xAxes: [
+        {
+          scaleLabel: {
+            display: true,
+            labelString: "Month",
+          },
         },
-    };
+      ],
+    },
+  };
 
+  const lineOptions = {
+    scales: {
+      yAxes: [
+        {
+          scaleLabel: {
+            display: true,
+            labelString: "Dollar Amount",
+          },
+        },
+      ],
+      xAxes: [
+        {
+          scaleLabel: {
+            display: true,
+            labelString: "Month",
+          },
+        },
+      ],
+    },
+  };
 
-    // useEffect(() => {
-    //     barData = fetch(`http://localhost:3001/api/bar`)
-    //         .then((response) => response.json())
-    //         .then((result) => {
-    //             console.log(result)
-    //             barData = result
-    //         })
-    // }, [])
+  // useEffect(() => {
+  //     barData = fetch(`http://localhost:3001/api/bar`)
+  //         .then((response) => response.json())
+  //         .then((result) => {
+  //             console.log(result)
+  //             barData = result
+  //         })
+  // }, [])
 
-
-
-    return (
-        <React.Fragment>
-            <Navbar />
-            {/* <PageBanner
+  return (
+    <React.Fragment>
+      <Navbar />
+      {/* <PageBanner
                 pageTitle="Charts"
                 breadcrumbTextOne="Home"
                 breadcrumbTextTwo="About Us"
                 breadcrumbUrl="/"
             /> */}
-            <div className="charts-div">
-                <Bar
-                    data={dataset}
-                    options={barOptions}
-                />
-                <Line
-                    data={lineData}
-                    options={lineOptions}    
-                />
-                <Pie data={pieData} />
-            </div>
-            <style jsx>
+      <div className="charts-div">
+        <Bar data={dataset} options={barOptions} />
+        <Line data={lineData} options={lineOptions} />
+        <Pie data={pieData} />
+      </div>
+      {/* <style jsx>
                 {`
                  .charts-div {
                     padding-top: 100px;
@@ -147,21 +137,21 @@ export default function Charts({ dataset }) {
 
                  }
                 `}
-            </style>
-            <Footer />
-        </React.Fragment>
-    )
+            </style> */}
+      <Footer />
+    </React.Fragment>
+  );
 }
 
 export async function getStaticProps() {
-    const res = await fetch(`http://localhost:3001/api/bar`)
-    const json = await res.json()
-    console.log(json)
-    return {
-        props: {
-            dataset: json
-        }
-    }
+  const res = await fetch(`http://localhost:3001/api/bar`);
+  const json = await res.json();
+  console.log(json);
+  return {
+    props: {
+      dataset: json,
+    },
+  };
 }
 
 // class Charts extends React.Component {
