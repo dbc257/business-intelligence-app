b38b0aee55eb21b7505f91af1ba6028715b0e26a
Victor Petsev <Vpetsev96@gmail.com>
2020-07-20 20:16:35 -0500

    companies route

---
+++ routes/companies.js
@@ -0,0 +1,13 @@
+let express = require("express");
+let router = express.Router();
+
+router.get("/", (req, res) => {
+    let companies = {
+        name: "apple",
+        name: "facebook",
+        name: "tesla"
+    }
+    res.json(companies)
+})
+
+module.exports = router
