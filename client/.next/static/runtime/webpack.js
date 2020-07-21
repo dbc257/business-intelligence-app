33ba5290c4fcf19e91a0856c0422105ab0c6746f
David Carlisle <dave_0021@yahoo.com>
2020-07-21 10:08:54 -0500

    added gitignore

--- client/.next/static/runtime/webpack.js
+++ client/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "1bc9827337902598adbd";
+/******/ 	var hotCurrentHash = "1bc72b7d943d8eb02048";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
